8786b9c7a6c96d41f6d7ddd79719d3e06dffc1b2
MUSTAFA IŞIK <mustafaisikk16@hotmail.com>
2020-12-22 14:46:53 +0300

    upload file

--- server/modules/files/FileRouter.js
+++ server/modules/files/FileRouter.js
@@ -1,8 +1,21 @@
 const FileController = require('./FileController')
 const router = require('express').Router()
+const path = require('path');
+const multer = require('multer');
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb)=>{
+    cb(null, './uploads/files')
+  },
+  filename: (req, file, cb)=>{
+    const filename = file.originalname.split('.')[0]
+    cb(null, filename+Date.now()+path.extname(file.originalname))
+
+  }
+})
+const upload = multer({storage})
 router.get('/',FileController.get_all)
 router.get('/file-count',FileController.file_count)
-router.post('/upload',FileController.upload)
+router.post('/upload',upload.single('file'),FileController.upload)
 
 module.exports = router
