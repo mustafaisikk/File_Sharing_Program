e70c5bed5e84de900dbdd5906c322bd683fec7d4
MUSTAFA IŞIK <mustafaisikk16@hotmail.com>
2020-12-22 15:58:08 +0300

    write from database

--- server/modules/files/FileController.js
+++ server/modules/files/FileController.js
@@ -1,13 +1,59 @@
 const FileModel = require('./FileModel')
 
 exports.get_all = (req,res) =>{
-  res.send('files')
+  FileModel.find()
+  .then(files=> {
+    res.status(200).json({
+      files
+    })
+  })
+  .catch(error => {
+    res.status(400).json({
+      message: 'Error',
+      error: error,
+      mongoose: error.errmsg
+    })
+  })
 }
 exports.upload = (req, res) =>{
-  res.send('upload')
-  console.log(req.file);
+  // res.send('upload')
+  const title = req.headers.title
+  const fileUrl = '/uploads/files/'+req.file.filename
+  const fileType = req.file.originalname.split('.')[1]
+
+  const newFile = new FileModel({
+    title, fileUrl, fileType
+  })
+  newFile.save()
+  .then(response => {
+    res.status(200).json({
+      message: 'Files uploaded succesfully',
+      id: response._id,
+      response
+    })
+  }).catch(error => {
+    res.status(400).json({
+      message: 'Error',
+      error : error,
+      mongoose: error.errmsg
+    })
+  })
+  // console.log(title+fileName+fileType);
 }
 
 exports.file_count = (req, res) =>{
-  res.send('file count')
+  const agg = [
+    {
+      $group : {
+        _id : '$fileType',
+        total: {$sum: 1}
+      }
+    }
+  ]
+  FileModel.aggregate(agg)
+  .then(response => {
+    res.status(200).json(
+      response
+    )
+  })
 }
